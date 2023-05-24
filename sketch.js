42430a36ed9a9a65c99717fb408b9d4b59c24c74
micaela-avi <micaelaavila340@gmail.com>
2023-05-24 11:15:22 -0300

    nueva actualizacion

---
+++ sketch.js
@@ -0,0 +1,55 @@
+function setup() {
+    createCanvas(400,400);
+    
+    background(100, 200, 150);
+    
+    
+    //lineas negras
+    line(330,130,400,130);//lineas horizontales
+    line(250,160,400,160);
+    line(350,200,400,200);
+    
+    line(220,20,270,90);//lineas diagonales
+    line (250,20,300,80);
+    
+    line(150,250,60,350);//lineas al lado de circulo pequeño
+    line (150,200,56,311); //Linea al lado de circulo pequeño
+    
+    line(300,40,100,100); //linea sola
+    line(80,80,300,300); //linea triangulo
+    line(200,100,300,300); //linea triangulo
+    
+    //circulos
+    fill(255,100,150,150);
+    noStroke();
+    ellipse(150,280,80,80);//rosa
+    
+    stroke(0);
+    fill(255);
+    ellipse(100,250,60,60);//blanco
+    
+    noStroke();
+    fill(500,150,0);
+    ellipse(100,250,50,50);//naranja
+    
+    fill(250,250,250);
+    noStroke();
+    ellipse(100,100,140,140);//verde
+    
+    fill(250,250,20);
+    noStroke();
+    ellipse(100,100,100,100);//amarillo
+    
+    //no pude usar la funcion para la grilla
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  }
+  
